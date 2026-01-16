3444040b5a55af868df404ab4b704d835f659de2
Gabriel Cavalheiro <gabriel.cavalheiromr@gmail.com>
2026-01-15 22:22:36 -0300

    feat: Initialize ZUI5 B2B NF

---
+++ webapp/controller/App.controller.ts
@@ -0,0 +1,12 @@
+import Controller from "sap/ui/core/mvc/Controller";
+
+/**
+ * @namespace br.com.inbetta.zui5b2bnf.controller
+ */
+export default class App extends Controller {
+
+    /*eslint-disable @typescript-eslint/no-empty-function*/
+    public onInit(): void {
+
+    }
+}
---
+++ webapp/controller/Main.controller.ts
@@ -0,0 +1,12 @@
+import Controller from "sap/ui/core/mvc/Controller";
+
+/**
+ * @namespace br.com.inbetta.zui5b2bnf.controller
+ */
+export default class Main extends Controller {
+
+    /*eslint-disable @typescript-eslint/no-empty-function*/
+    public onInit(): void {
+
+    }
+}
